28b2ab8845042f32f5a36d721b25a6372b0f85ef
imzhyke <punk1313444@gmail.com>
2025-01-02 04:32:32 +0800

    Font & Assets

---
+++ src/app/about.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const About = () => {
+  return (
+    <View className=" bg-red-300 items-center justify-center h-64">
+      <Text>About</Text>
+    </View>
+  )
+}
+
+export default About
+
+const styles = StyleSheet.create({})
