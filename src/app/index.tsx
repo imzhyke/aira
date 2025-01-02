4c3afe47cf38f6c68410bc874ff859ba559c3240
imzhyke <punk1313444@gmail.com>
2025-01-03 04:41:41 +0800

    Onboarding Screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,64 +3,67 @@ import { Redirect, router } from "expo-router";
 import { View, Text, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { images } from "../constants";
-// import { CustomButton, Loader } from "../components";
-// import { useGlobalContext } from "../context/GlobalProvider";
+import {images} from '../constants'
+import CustomButton from "@/components/CustomButton";
 
 const Welcome = () => {
-  // const { loading, isLogged } = useGlobalContext();
-
-  // if (!loading && isLogged) return <Redirect href="/home" />;
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      {/* <Loader isLoading={loading} /> */}
-
-      <ScrollView
-        contentContainerStyle={{
-          height: "100%",
-        }}
-      >
-        <View className="w-full flex justify-center items-center h-full px-4">
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className='w-full justify-center items-center min-h-[85vh] px-4 '>
           <Image
             source={images.logo}
-            className="w-[130px] h-[84px]"
+            style={{
+              width: 130,
+              height: 84,
+            }}
             resizeMode="contain"
           />
 
           <Image
             source={images.cards}
-            className="max-w-[380px] w-full h-[298px]"
+            style={{
+              maxWidth: 380,
+              width: '100%',
+              height: 300
+            }}
             resizeMode="contain"
           />
 
           <View className="relative mt-5">
-            <Text className="text-3xl text-white font-bold text-center">
-              Discover Endless{"\n"}
-              Possibilities with{" "}
-              <Text className="text-secondary-200">Aora</Text>
-            </Text>
-
+            <Text className="text-3xl text-white  text-center font-pbold">
+            Discover a World Without Limits with{' '}
+              <Text className='text-secondary-200'> AiRa </Text>
+           </Text>
+            
             <Image
               source={images.path}
-              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
-              resizeMode="contain"
+              style={{
+                width: 136,
+                height: 15
+              }}
+              className="absolute -bottom-2 -right-7"
+              resizeMode="contain"  
             />
-          </View>
+          
+            </View>
 
-          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
-            Where Creativity Meets Innovation: Embark on a Journey of Limitless
-            Exploration with Aora
-          </Text>
+            <Text className="font-pregular text-sm text-gray-100 mt-7 text-center">
+              Embark on Boundless Adventures: Creativity and Innovation Together with AiRa
+            </Text>
+          
 
-          {/* <CustomButton
+          <CustomButton
             title="Continue with Email"
             handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
-          /> */}
+            textStyles={undefined}
+            isLoading={undefined}
+          />
+
         </View>
       </ScrollView>
-
       <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
